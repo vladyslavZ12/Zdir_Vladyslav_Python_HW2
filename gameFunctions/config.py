bf701f8f90d9ecc61d74ea46923f02452d1598ed
Zdir <v_zdir@ats-hr420-dm34.fc.ca>
2019-11-14 21:25:36 -0500

    finished code

---
+++ gameFunctions/config.py
@@ -0,0 +1,13 @@
+from random import randint
+
+
+player_lives = 5
+computer_lives = 5
+total_lives = 5
+
+
+choices = ["rock","paper","scissors"]
+
+computer = choices [randint(0, 2)]
+
+player = False
